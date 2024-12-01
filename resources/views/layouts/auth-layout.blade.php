09a43c3a0f0470fef8ef81d7b924f95f2c740027
Victor Halim <victorhalim@192.168.1.17>
2024-12-01 15:25:44 +0700

    ui: login page

--- resources/views/layouts/auth-layout.blade.php
+++ resources/views/layouts/auth-layout.blade.php
@@ -6,19 +6,17 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{{ csrf_token() }}}">
     <title>@yield('title')</title>
-    <link rel="stylesheet" href="{{ asset('bootstrap-5.3.3-dist/css/bootstrap.css') }}">
     <link rel="stylesheet" href={{asset('css/app.css')}}>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href={{asset('css/auth.css')}}>
+    <link rel="stylesheet" href="{{ asset('bootstrap-5.3.3-dist/css/bootstrap.css') }}">
+    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/css/auth.css'])
 </head>
 <body>
-    @include('partials.navigation-bar')
     <div class="main-container">
         @yield('content')
     </div>
-    @include('partials.footer')
     @yield('scripts')
-    <script src="{{ asset('bootstrap-5.3.3-dist/js/bootstrap.bundle.js') }}"></script>
-    <script src="https://kit.fontawesome.com/f7c4eeb796.js" crossorigin="anonymous"></script>
 </body>
-
+<script src="{{ asset('bootstrap-5.3.3-dist/js/bootstrap.bundle.js') }}"></script>
+<script src="https://kit.fontawesome.com/f7c4eeb796.js" crossorigin="anonymous"></script>
 </html>
--- resources/views/login-page.blade.php
+++ resources/views/login-page.blade.php
@@ -1,29 +1,59 @@
-@extends('layouts.main-layout')
-
+@extends('layouts.auth-layout')
 
 @section('title', 'Login To TrustSphere')
 
 @section('content')
-    <div>
-        <form action="{{ route('login')}}" method="POST">
-            @csrf
-            <div class="form-group">
-              <label for="email">Email address</label>
-              <input type="email" class="form-control" id="email" aria-describedby="emailHelp" name="email" placeholder="Enter email">
-              <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+
+    <div class="content">
+        <section class="form-container">
+            <h1 class="content-header">TrustSphere.</h1>
+            <form class="form-section" action="{{ route('login') }}" method="POST">
+                @csrf
+                <h2 class="form-header">Sign In</h2>
+                <div class="form-group">
+                    <label class="input-label" for="email">Email</label>
+                    <div class="input-wrapper">
+                        <i class="fa-regular fa-envelope icon"></i>
+                        <input class="input-field" type="email" id="email" name="email" placeholder="Enter email">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="input-label" for="password">Password</label>
+                    <div class="input-wrapper">
+                        <i class="fa-solid fa-lock icon"></i>
+                        <input class="input-field" type="password" id="password" name="password" placeholder="Password">
+                    </div>
+                </div>
+
+                @error('email')
+                    <small class="text-danger">{{ $message }}</small>
+                @enderror
+
+                <button class="submit-button" type="submit">Sign In</button>
+
+                <div class="divider">
+                    <span>Or</span>
+                </div>
+
+                <button class="register-button" type="button"
+                    onclick="window.location.href='{{ route('register') }}'">Register Now</button>
+            </form>
+        </section>
+        <section class="promotion-container">
+            <p class="promo-header">Connecting ideas with opportunities, turning visions into reality.</p>
+            <div class="promo-achievements">
+                <div class="achievement">
+                    <h3 class="achievement-value">100+</h3>
+                    <p class="achievement-label">Projects Funded</p>
+                </div>
+                <div class="separator">|</div>
+                <div class="achievement">
+                    <h3 class="achievement-value">$50,000+</h3>
+                    <p class="achievement-label">Funded to Projects</p>
+                </div>
+
             </div>
-            <div class="form-group">
-              <label for="password">Password</label>
-              <input type="password" class="form-control" id="password" name="password" placeholder="Password">
-            </div>
-            <div class="form-group form-check">
-              <input type="checkbox" class="form-check-input" id="exampleCheck1">
-              <label class="form-check-label" for="exampleCheck1">Check me out</label>
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-            @error('email')
-                <small class="text-danger">{{ $message }}</small>
-            @enderror
-          </form>
+        </section>
     </div>
+
 @endsection
