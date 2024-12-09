c010ac14b46440b6c6b11d90ca3340b18f189e97
victorhalimm <victorhalim18@gmail.com>
2024-12-09 23:32:04 +0700

    fix: fix status logic for project

--- resources/views/partials/project-card.blade.php
+++ resources/views/partials/project-card.blade.php
@@ -24,7 +24,7 @@
                 @endforeach
             </div>
         </div>
-        @if ($project->is_safe)
+        @if (!$project->is_safe)
         <div class="esclamation-text-container">
             <svg class="warning-icon w-5 h-5 text-gray-800 dark:text-white" aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
